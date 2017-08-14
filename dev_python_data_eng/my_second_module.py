41cd30e475144321ba721de915d508d1471bc8aa
Dongwan Kim <notyetend@gmail.com>
2017-08-14 09:55:20 +0900

    no message

---
+++ dev_python_data_eng/my_second_module.py
@@ -0,0 +1,17 @@
+val_mod = 'Hello world'
+
+print(__name__)
+
+def my_func_one(n):    # write Fibonacci series up to n
+    print('my_func_two')
+    
+def my_func_two(n):   # return Fibonacci series up to n
+    print(n)
+
+def print_my_name():
+    print(__name__)
+
+class MyDog:
+    def __init__(name):
+        self.name = name
+        print('doggy ~', self.name)
