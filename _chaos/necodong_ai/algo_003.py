b05867df0441514423f7b07315c18fbb8e0b5181
Dongwan Kim <a>
2019-10-16 15:58:16 +0900

    .

--- _chaos/necodong_ai/algo_003.py
+++
@@ -1,7 +0,0 @@
-def think(hands, history, old_games):
-    import random
-
-    def random_choose(val_list):
-        return val_list[int(random.uniform(0, len(val_list)))]
-
-    return random_choose(hands)
